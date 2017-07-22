ec09c3fce9e061bb78b19095ff28158d039ce657
zhangjialegh <zjl1195185765@outlook.com>
2017-07-22 15:50:10 +0800

    port

--- app.js
+++ app.js
@@ -20,7 +20,19 @@ var app = express();
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+  }
+}
 
+app.use(express.static(path.join(__dirname, 'public'), options));
 // app.use('/', index);
 // app.use('/users', users);
 
