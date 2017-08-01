f7a45f6ca7f3c1dbfe4800c5c2ec65c5cdf21324
zhangjialegh <zjl1195185765@outlook.com>
2017-08-01 21:57:36 +0800

    add gzip

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var compression = require('compression');
 var path = require('path');
 // var favicon = require('serve-favicon');
 // var logger = require('morgan');
@@ -19,6 +20,7 @@ var app = express();
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 var options = {
   dotfiles: 'ignore',
